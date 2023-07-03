585d266cfcb2ee0fc0e8b3ea7b0506d665d790f4
Amelia Chapman <ameliachapman@amelias-air.devices.brown.edu>
2023-07-02 23:58:07 -0400

    progresssss

--- social_network_classes.py
+++ social_network_classes.py
@@ -1,8 +1,9 @@
 # A class to hold general system wide social media data and functions. Eg Data objects of all people, Eg functions: Save social media to disk
 class SocialNetwork:
     def __init__(self):
-        self.list_of_people = [] # this instance variable is initialized to an empty list when social network is created, 
+        self.listPeople = [] # this instance variable is initialized to an empty list when social network is created, 
                                  # you can save objects of people on the network in this list
+        self.listUsernames = []
         
     ## For more challenge try this
     def save_social_media(self):
@@ -20,18 +21,34 @@ class SocialNetwork:
     def  create_account(self):
         #implement function that creates account here
         print("Creating ...")
-        pass
+        username = input("Enter a username: ")
+        name = input("Enter your name: ")
+        age = input("Enter your age: ")
+        favColor = input("Enter your favorite color: ")
+        favAnimal = input("Enter your favorite animal: ")
+        p1 = Person(username,name,age,favColor,favAnimal)
+        print(f"Your name is {p1.name} and your age is {p1.age}. Your favorite color is {p1.favColor} and your favorite animal is a {p1.favAnimal}. Write down your username, {p1.username}, so you can log back in later.")
+        self.listPeople.append(p1)
+        self.listUsernames.append(p1.username)
+
 
 
 class Person:
-    def __init__(self, name, age):
-        self.id = name
-        self.year = age
+    def __init__(self, username, name, age, favColor, favAnimal):
+        self.username = username
+        self.name = name
+        self.age = age
+        self.favColor = favColor
+        self.favAnimal = favAnimal
         self.friendlist = []
+        self.blockedFriends = []
+        self.inbox = []
 
     def add_friend(self, person_object):
-        #implement adding friend. Hint add to self.friendlist
-        pass
+        person_object = input("Enter the name of a friend to add: ")
+        self.friendlist.append(person_object)
+
+        
 
     def send_message(self):
         #implement sending message to friend here
