2cb0ad33c43fe971695142c02009ae5e81539335
Amelia Chapman <ameliachapman@amelias-air.devices.brown.edu>
2023-07-04 12:09:14 -0400

    social network ui finished

--- social_network_ui.py
+++ social_network_ui.py
@@ -9,32 +9,38 @@ def loginMenu():
     print("2. Login")
     print("3. Quit")
     print("********************************************************")
-    return input("Please Choose a number: ")
+    return input("Please choose a number: ")
 
 def manageAccountMenu():
     print("")
     print("\nYou are now in the menu to manage your account")
     print("\n1. Edit my details")
     print("2. Manage my friends")
-    print("3. View my messages")
+    print("3. Messages")
     print("4. Logout")
-    return input("Please Choose a number: ")
+    return input("Please choose a number: ")
 
 def editDetailsMenu():
-    print("")
-    print("\nYou are now in the menu to edit your details")
     print("\n1. Edit name")
     print("2. Edit age")
     print("3. Edit favorite color")
     print("4. Edit favorite animal")
     print("5. <- Go back")
-    return input("Please Choose a number: ")
+    return input("Please choose a number: ")
 
 def manageFriendsMenu():
     print("")
-    print("\nYou are now in the menu to manage your frienfs")
+    print("\nYou are now in the menu to manage your friends")
     print("\n1. Add a friend")
     print("2. View all my friends")
     print("3. Block a friend")
     print("4. <- Go back")
+    return input("Please choose a number: ")
+
+def messagesMenu():
+    print("")
+    print("\nYou are now in the menu to send and recieve messages")
+    print("\n1. Send a message")
+    print("2. View inbox")
+    print("3. <- Go back")
     return input("Please choose a number: ")
